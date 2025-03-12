62f0b20ad8cd9293877a91a2c5e2f2507aacee29
Pedro Henrique <pedrohenriquesnows22@gmail.com>
2025-03-12 09:45:33 -0300

    Model created

---
+++ to-do-list_Simplify/src/main/java/com/example/to_do_list_Simplify/models/Task.java
@@ -0,0 +1,29 @@
+package com.example.to_do_list_Simplify.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "TB_tasks")
+public class Task implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idTask;
+    private String name;
+    private String description;
+
+    @Column(nullable = true)
+    private boolean carriedOut;
+    private String priority;
+}
