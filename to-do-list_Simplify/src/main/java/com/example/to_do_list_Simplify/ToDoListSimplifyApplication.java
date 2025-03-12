d96e0f3216eaa44c6d230a6e8ece2199e6ccba55
Pedro Henrique <pedrohenriquesnows22@gmail.com>
2025-03-12 00:07:17 -0300

    First commit

---
+++ to-do-list_Simplify/src/main/java/com/example/to_do_list_Simplify/ToDoListSimplifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.to_do_list_Simplify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListSimplifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListSimplifyApplication.class, args);
+	}
+
+}
---
+++ to-do-list_Simplify/src/test/java/com/example/to_do_list_Simplify/ToDoListSimplifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.to_do_list_Simplify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListSimplifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
