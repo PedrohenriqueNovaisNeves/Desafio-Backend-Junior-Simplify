51e09fe6b1fe377742d43dc6f1cf70be595a87f2
Pedro Henrique <pedrohenriquesnows22@gmail.com>
2025-03-13 09:15:25 -0300

    Lombok added

--- to-do-list_Simplify/src/main/java/com/example/to_do_list_Simplify/models/TaskModel.java
+++ to-do-list_Simplify/src/main/java/com/example/to_do_list_Simplify/models/TaskModel.java
@@ -9,7 +9,6 @@ import lombok.Setter;
 import java.io.Serializable;
 import java.util.UUID;
 
-@AllArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -29,46 +28,6 @@ public class TaskModel implements Serializable {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Priority priority;
-
-    public UUID getIdTask() {
-        return idTask;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public boolean isCarriedOut() {
-        return carriedOut;
-    }
-
-    public Priority getPriority() {
-        return priority;
-    }
-
-    public void setIdTask(UUID idTask) {
-        this.idTask = idTask;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setCarriedOut(boolean carriedOut) {
-        this.carriedOut = carriedOut;
-    }
-
-    public void setPriority(Priority priority) {
-        this.priority = priority;
-    }
 
     public void setStatus(String status){
         if(status.equals("Realizado")){
